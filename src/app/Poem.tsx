6e0f01ba5f32b33236e9d58f542c359a7e516f11
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2023-06-28 22:10:28 -0700

    Updated home page

---
+++ src/app/Poem.tsx
@@ -0,0 +1,37 @@
+export default function Poem() {
+	return (
+		<p className='text-base sm:text-norm font-extralight'>
+			Along the shore the cloud waves break,
+			<br />
+			The twin suns sink behind the lake,
+			<br />
+			The shadows lengthen
+			<br />
+			<span className='block w-32 ml-auto'>In Carcosa.</span>
+			<br />
+			Strange is the night where black stars rise,
+			<br />
+			And strange moons circle through the skies
+			<br />
+			But stranger still is
+			<br />
+			<span className='block w-32 ml-auto'>Lost Carcosa.</span>
+			<br />
+			Songs that the Hyades shall sing,
+			<br />
+			Where flap the tatters of the King,
+			<br />
+			Must die unheard in
+			<br />
+			<span className='block w-32 ml-auto'>Dim Carcosa.</span>
+			<br />
+			Song of my soul, my voice is dead;
+			<br />
+			Die thou, unsung, as tears unshed
+			<br />
+			Shall dry and die in
+			<br />
+			<span className='block w-32 ml-auto'>Lost Carcosa.</span>
+		</p>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,24 @@
 import Link from 'next/link';
-import { getEntry, insertEntry } from './kysely/database';
+import Poem from './Poem';
 
 export default async function Page() {
-	// const entry = {
-	// 	title: 'The Red Eye',
-	// 	part: '',
-	// 	place: [''],
-	// 	text: [
-	// 		"There exists a strange, folklorish idea in many sectors of Rotfront, related to 'pareidolia', the tendency to see meaningful images in random patterns (like seeing a face in an inkblot, or letters in a smudge of dirt).\n It is well-known that the so-called 'red eye' is simply an anticyclonic storm produced by a high-pressure region in the atmosphere of Rotfront's planet.",
-	// 		'Yet for some of the early settlers of Rotfront this natural phenomenon became symbolic of their struggle and way of life. That red spot in the sky became a perfect metaphor for the ever-present surveillance by the Protektors and the tight grip of the central government on Heimat. Even today, the idea of an unblinking, watchful eye observing their every move still resonates strongly with the people here.',
-	// 		"During the celebration of Mondfest at the end of each Season, adult citizens will\n sometimes give students Rationmarks that have been dipped in red paint. Officially, the\n red paint is said to represent the blood of those who died in the revolution - but the red coins share an eerie resemblance to that red eye - Or is this just 'pareidolia', too?",
-	// 		"Should you ever recive one of these 'red eye' Rationmarks, remember to clean off the paint with Acetone or a similar paint thinner. Spending or re-gifting paint-covered coins is considered to bring bad luck.",
-	// 	],
-	// 	tags: [''],
-	// };
-	// await insertEntry(entry)
 	return (
 		<>
 			<h1 className="mb-2 mt-12 text-3xl font-semibold sm:mb-4 sm:mt-24 sm:text-4xl">Welcome</h1>
-			<p className="mb-12 text-base sm:text-norm">
-				This is an unofficial site for searching memories in{' '}
+			<p className="mb-4 text-base sm:text-norm max-w-2xl">
+				This is an unofficial, fan-made site for searching memories in{' '}
 				<a className="text-primary-blue" href="https://store.steampowered.com/app/1262350/SIGNALIS/">
 					Signalis
 				</a>
-				.
+				. I hope that this will help players dig into the lore and discuss it more easily with each other.
 			</p>
 			<Link
 				href="/entries"
-				className="bg-primary-orange px-8 py-1 text-base font-medium hover:bg-off-white hover:text-black sm:text-norm"
+				className="bg-primary-orange mb-12 px-8 py-1 text-base font-medium hover:bg-off-white hover:text-black sm:text-norm"
 			>
 				Browse Entries
 			</Link>
+			<Poem />
 		</>
 	);
 }
