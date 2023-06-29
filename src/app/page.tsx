90e9df7f878f5c9eae1247f5a9dbd617b2598dee
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2023-06-29 01:00:31 -0700

    Added animation to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Poem from './Poem';
 export default async function Page() {
 	return (
 		<>
-			<h1 className="mb-2 mt-12 text-3xl font-semibold sm:mb-4 sm:mt-24 sm:text-4xl">Welcome</h1>
-			<p className="mb-4 text-base sm:text-norm max-w-2xl">
+			<h1 className="fly-right-fade mb-2 mt-12 text-3xl font-semibold sm:mb-4 sm:mt-24 sm:text-4xl">Welcome</h1>
+			<p className="fly-right-fade mb-4 text-base sm:text-norm max-w-2xl" style={{ animationDelay: '50ms' }}>
 				This is an unofficial, fan-made site for searching memories in{' '}
 				<a className="text-primary-blue" href="https://store.steampowered.com/app/1262350/SIGNALIS/">
 					Signalis
@@ -13,8 +13,8 @@ export default async function Page() {
 				. I hope that this will help players dig into the lore and discuss it more easily with each other.
 			</p>
 			<Link
-				href="/entries"
-				className="bg-primary-orange mb-12 px-8 py-1 text-base font-medium hover:bg-off-white hover:text-black sm:text-norm"
+				href="/entries" style={{ animationDelay: '100ms' }}
+				className="fly-right-fade bg-primary-orange mb-12 px-8 py-1 text-base font-medium hover:bg-off-white hover:text-black sm:text-norm"
 			>
 				Browse Entries
 			</Link>
