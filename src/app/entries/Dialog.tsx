f60e638d257011d8fe8b2ab81747149296a121f3
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2024-07-23 09:55:10 -0700

    Fixed padding

--- src/app/entries/Dialog.tsx
+++ src/app/entries/Dialog.tsx
@@ -59,7 +59,7 @@ export default function Dialog() {
 			<dialog
 				ref={dialogRef}
 				onClick={clickHandler}
-				className="px-2 max-w-5xl w-full mt-2 sm:mt-16 bg-transparent backdrop:cursor-pointer backdrop:bg-zinc-950/90"
+				className="p-2 max-w-5xl w-full mt-2 sm:mt-16 bg-transparent backdrop:cursor-pointer backdrop:bg-zinc-950/90"
 			>
 				<SearchInput clickHandler={clickHandler} />
 			</dialog>
