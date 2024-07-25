4302959ef9e6f672116b2c3b36395d3d5254df43
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2024-07-24 19:48:43 -0700

    Updated pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,20 @@ import Poem from './Poem';
 export default async function Page() {
 	return (
 		<>
-			<h2 className='mt-2 font-light text-sm text-white/80 text-center'>-- This site has been updated for version 1.2 of Signalis --</h2>
+			<h2 className="mt-2 font-light text-sm text-white/80 text-center">
+				-- This site has been updated for version 1.2 of Signalis --
+			</h2>
 			<h1 className="fly-right-fade mb-2 mt-6 text-3xl font-semibold sm:mb-4 sm:mt-16 sm:text-4xl">Welcome</h1>
 			<p className="fly-right-fade mb-4 text-base sm:text-norm max-w-2xl" style={{ animationDelay: '50ms' }}>
-				This is an unofficial, fan-made site for searching memories in{' '}
-				<a
-					className="text-primary-blue hover:underline hover:text-primary-blue-hover"
-					href="https://store.steampowered.com/app/1262350/SIGNALIS/"
-				>
-					Signalis
-				</a>
+				This is an unofficial, fan-made site for searching memories in the game:{' '}
+				<strong>
+					<a
+						className="text-primary-blue hover:underline hover:text-primary-blue-hover"
+						href="https://store.steampowered.com/app/1262350/SIGNALIS/"
+					>
+						Signalis
+					</a>
+				</strong>
 				. I hope that this will help players dig into the lore and discuss it more easily with each other.
 			</p>
 			<a
