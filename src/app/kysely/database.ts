f2c846d12a959c543a158e032d168c5541b4d242
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2024-07-24 17:42:00 -0700

    Updated query

--- src/app/kysely/database.ts
+++ src/app/kysely/database.ts
@@ -19,11 +19,10 @@ export async function getEntry(id: string) {
 			.select(['entry.id', 'entry.title'])
 			.innerJoin('text', 'text.entryid', 'entry.id')
 			.select(() => [sql<string[]>`array_agg(text.text ORDER BY text.page)::text[]`.as('text')])
-			.innerJoin('place', 'place.entryid', 'entry.id')
-			.select(() => [sql<string[]>`array_agg(DISTINCT place)::text[]`.as('place')])
 			.where('entry.slug', '=', id)
 			.groupBy(['entry.id'])
 			.executeTakeFirstOrThrow();
+
 		return entry;
 	} catch (error) {
 		console.error(error);
