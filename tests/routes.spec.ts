60b58e5e511aca5eadb5be1e9a9d2d80cc5d84ba
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2024-08-20 19:37:06 -0700

    Added testing

---
+++ tests/routes.spec.ts
@@ -0,0 +1,50 @@
+import { notFound } from 'next/navigation';
+import { test, expect } from '@playwright/test';
+import { getAllEntrySlugs, getEntry } from '@/app/lib/transcriptions';
+
+//TODO test.describe
+test('should navigate to browse page', async ({ page }) => {
+	await page.goto('/');
+
+	await page.click('a:has-text("Browse Entries")');
+
+	await expect(page).toHaveURL('/entries');
+});
+
+test('slugs should replace space with dash', () => {
+	const slugs = getAllEntrySlugs();
+	slugs.forEach((slug) => {
+		const entry = getEntry(slug);
+		expect(entry).toBeTruthy();
+
+		const title = entry?.browseTitle || entry?.title;
+		expect(title).toBeTruthy();
+
+		const expectedSlug = title?.replaceAll(' ', '-');
+		expect(slug).toBe(expectedSlug);
+	});
+});
+
+test('browse should have all entries', async ({ page }) => {
+	await page.goto('/entries');
+
+	const accordionLinks = await page.locator('ul li a').all();
+	const hrefs = await Promise.all(accordionLinks.map((link) => link.getAttribute('href')));
+
+	const slugs = getAllEntrySlugs().map((slug) => `/entries/${slug}`);
+	expect(slugs.length).toBe(hrefs.length);
+
+	hrefs.forEach((href) => {
+		expect(slugs).toContain(href);
+	});
+});
+
+test('all entries should be found', async ({ page }) => {
+	test.setTimeout(120000)
+	const slugs = getAllEntrySlugs();
+	for (const slug of slugs) {
+		await page.goto(`/entries/${slug}`);
+		const title = await page.locator('h1').textContent();
+		expect(title).not.toBe('404');
+	}
+});
