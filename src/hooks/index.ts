b313300793c54f6ce7a7877378da035f2cc1cd19
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2024-09-03 16:06:40 -0700

    Added useDebounce and useIntersectionObserver hooks
    Fixes race condition

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,9 @@ import { useDialog } from './useDialog';
 import { useKeyDown } from './useKeyDown';
 import { useMediaQuery } from './useMediaQuery';
 import { usePagination } from './usePagination';
+import { useDebouncedFunction } from './useDebounce';
+import { useIntersectionObserver, IntersectionObserverCallback } from './useIntersectionObserver';
 
-export { useDialog, useKeyDown, useMediaQuery, usePagination };
+export { useDialog, useKeyDown, useMediaQuery, usePagination, useDebouncedFunction, useIntersectionObserver };
+
+export type { IntersectionObserverCallback };
